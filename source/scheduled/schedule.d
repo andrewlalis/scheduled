fe50eb1b0cfe91b3bbf52f0688f1a2b93cea6c2d
Andrew Lalis <andrewlalisofficial@gmail.com>
2021-09-22 20:13:01 +0200

    Added lots of documentation.

--- source/scheduled/schedule.d
+++ source/scheduled/schedule.d
@@ -13,8 +13,16 @@ public interface JobSchedule {
      */
     Nullable!SysTime getNextExecutionTime(SysTime currentTime);
 
+    /**
+     * Marks the schedule as having been executed at the given time.
+     */
     void markExecuted(SysTime executionTime);
 
+    /**
+     * Tells whether the schedule is repeating; that jobs with this schedule
+     * should be re-queued after being executed.
+     * Returns: True if this schedule is repeating, or false otherwise.
+     */
     bool isRepeating();
 }
 
@@ -22,15 +30,27 @@ public interface JobSchedule {
  * Simple wrapper interface for obtaining the current system time.
  */
 public interface CurrentTimeProvider {
+    /** 
+     * Gets the current system time.
+     * Returns: The current system time.
+     */
     SysTime now() const;
 }
 
+/** 
+ * Standard implementation of the current time provider, which simply returns
+ * the current system time.
+ */
 public class SysTimeProvider : CurrentTimeProvider {
     SysTime now() const {
         return Clock.currTime;
     }
 }
 
+/** 
+ * Implementation of the current time provider which always returns a fixed
+ * value, useful for testing.
+ */
 public class FixedTimeProvider : CurrentTimeProvider {
     private const SysTime currentTime;
 
--- source/scheduled/schedules/one_time.d
+++ source/scheduled/schedules/one_time.d
@@ -6,33 +6,71 @@ import scheduled.schedule;
 
 /** 
  * Schedule which defines a job to be executed once at a fixed time.
+ *
+ * Note that if a job is scheduled with this schedule, and the execution time
+ * is set to a time in the past, the job will be executed immediately.
  */
 public class OneTimeSchedule : JobSchedule {
+    /**
+     * The time at which the job will be executed.
+     */
     private SysTime executionTime;
+
+    /** 
+     * Simple flag to indicate whether the job has already been executed. This
+     * is useful because 
+     */
     private bool executed = false;
 
+    /** 
+     * Constructs a one-time schedule to execute at the specified time.
+     * Params:
+     *   executionTime = The time to execute the job at.
+     */
     this(SysTime executionTime) {
         this.executionTime = executionTime;
     }
 
+    /** 
+     * Constructs a one-time schedule to execute after the given duration has
+     * passed, from the current system time.
+     * Params:
+     *   timeUntilExecution = The time from now until execution.
+     */
     this(Duration timeUntilExecution) {
         this(Clock.currTime + timeUntilExecution);
     }
 
+    /** 
+     * Gets the next execution time. If this one-time schedule has already
+     * executed once, it will always return null.
+     * Params:
+     *   currentTime = The current system time.
+     * Returns: The time at which jobs with this schedule should next be
+     * executed, or null if there is no known next execution time.
+     */
     Nullable!SysTime getNextExecutionTime(SysTime currentTime) const {
-        Nullable!SysTime result;
         if (this.executed) {
-            result.nullify;
+            return Nullable!SysTime.init;
         } else {
-            result = this.executionTime;
+            return nullable!SysTime(this.executionTime);
         }
-        return result;
     }
 
+    /** 
+     * Marks this schedule as having been executed. Once this is called, no
+     * more jobs with this schedule will be executed.
+     * Params:
+     *   executionTime = The time at which this schedule's job was executed.
+     */
     void markExecuted(SysTime executionTime) {
         this.executed = true;
     }
 
+    /** 
+     * Tells the scheduler that this schedule is never repeating.
+     * Returns: Always false.
+     */
     bool isRepeating() {
         return false;
     }
